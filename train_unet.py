47038b37774ac2f09fafd7b476bf1a9fbc7418bf
omosan0627 <hodakaymj0627@yahoo.co.jp>
2017-09-02 00:47:58 +0900

    organized

--- train_unet.py
+++ train_unet.py
@@ -1,16 +1,14 @@
 import tensorflow as tf
 import numpy as np
-import os
 import matplotlib.pyplot as plt
 import pickle
-import shutil
+import os
+
 import batch
-from collections import OrderedDict
-from libs import (get_variable, get_conv, get_bias, get_pool, get_crop, get_concat, get_deconv2)
+import unet #UNETをimportします
 
 np.random.seed(1919114)
 tf.set_random_seed(1919114)
-
 
 with open('data/train_x', 'rb') as f:
     image_x = pickle.load(f)
@@ -18,116 +16,9 @@ with open('data/train_x', 'rb') as f:
 with open('data/train_t', 'rb') as f:
     image_t = pickle.load(f)
 
-input_sizex = 572   #input_sizex % 16 == 12ののものなら何でもよい
-input_sizey = 588   #こっちも
-output_sizex = input_sizex - 184
-output_sizey = input_sizey - 184
-num_class = 2
 
-class UNET: #画像はテストデータラベルともにフラットにして入力
-    #テストデータ[-1, input_sizex, input_sizey]
-    #ラベル[-1, input_sizex, input_sizey, 2]
-    def __init__(self):
-        with tf.Graph().as_default():
-            self.prepare_model()
-            self.prepare_session()
-
-    def prepare_model(self):
-        with tf.name_scope('input'):
-            x = tf.placeholder(tf.float32, [None, input_sizex, input_sizey])
-            h_pool = tf.reshape(x, [-1, input_sizex, input_sizey, 1])
-
-        with tf.name_scope('contracting'):
-            depth = 4
-            b = 8  #層を多くする。論文は64でやってる。
-            h_array = OrderedDict()
-
-            for i in range(depth):
-                if i == 0:
-                    filter1 = get_variable([3, 3, 1, b])
-                else:
-                    filter1 = get_variable([3, 3, b // 2, b])
-                h1 = get_conv(h_pool, filter1, 1, 'VALID')
-
-                filter2 = get_variable([3, 3, b, b])
-                h2 = get_conv(h1, filter2, 1, 'VALID')
-
-                h_array[i] = h2
-                h_pool = get_pool(h2, 2)
-                b = b * 2
-
-        with tf.name_scope('floor'):
-            filter5_1 = get_variable([3, 3, b // 2, b])
-            h5_1 = get_conv(h_pool, filter5_1, 1, 'VALID')
-
-            filter5_2 = get_variable([3, 3, b, b])
-            h_pool = get_conv(h5_1, filter5_2, 1, 'VALID')
-
-
-        with tf.name_scope('expanding'):
-            for i in range(depth):
-                filter5 = get_variable([2, 2, b // 2, b])
-                h3 = get_deconv2(h_pool, filter5)
-
-                hcat = get_concat(h_array[depth - 1- i], h3)
-                filter1 = get_variable([3, 3, b, b // 2])
-                h1 = get_conv(hcat, filter1, 1, 'VALID')
-
-                filter2 = get_variable([3, 3, b // 2, b // 2])
-                h_pool = get_conv(h1, filter2, 1, 'VALID')
-
-                b = b // 2
-
-        with tf.name_scope('dropout'):
-            keep_prob = tf.placeholder(tf.float32)
-            h_pool = tf.nn.dropout(h_pool, keep_prob)
-
-        with tf.name_scope('softmax'):
-
-            filter1_3 = get_variable([1, 1, b, num_class])
-            h_pool = get_conv(h_pool, filter1_3, 1, 'VALID') 
-
-            h_pool_flat = tf.reshape(h_pool, [-1, num_class])
-
-            result = tf.nn.softmax(h_pool_flat)
-
-        with tf.name_scope('optimizer'):
-            t = tf.placeholder(tf.float32, [None, input_sizex, input_sizey, num_class])
-            tcrop = get_crop(t, [output_sizex, output_sizey])
-            tout = tf.reshape(tcrop, [-1, num_class])
-            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tout,logits=result))
-            train_step = tf.train.MomentumOptimizer(learning_rate = 0.02, momentum = 0.02).minimize(loss)
-
-        with tf.name_scope('evaluator'):
-            correct_prediction = tf.equal(tf.argmax(result, 1), tf.argmax(tout, 1))
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-        tf.summary.scalar("loss", loss)
-        tf.summary.scalar("accuracy", accuracy)
-        
-        self.x, self.t, self.result, self.keep_prob = x, t, result, keep_prob
-        self.train_step = train_step
-        self.loss = loss
-        self.tout = tout
-        self.result = result
-        self.accuracy = accuracy
-
-    def prepare_session(self):
-        sess = tf.InteractiveSession()
-        sess.run(tf.global_variables_initializer())
-        summary = tf.summary.merge_all()
-
-        saver = tf.train.Saver()
-        if os.path.isdir('/tmp/logs'):
-            shutil.rmtree('/tmp/logs')
-        writer = tf.summary.FileWriter("/tmp/logs", sess.graph)
-        
-        self.sess = sess
-        self.summary = summary
-        self.writer = writer
-        self.saver = saver
-
-unet = UNET()
+#UNETを初期化しています。
+unet = unet.UNET(572, 588, 2, depth = 4, layers_default = 8)
 
 i = 0
 Batch_x = batch.Batch(image_x)
@@ -139,15 +30,23 @@ for _ in range(1):
     batch_t = Batch_t.next_batch(10)
     unet.sess.run(unet.train_step,
             feed_dict={unet.x:image_x, unet.t:image_t, unet.keep_prob:0.1})
-    if i % 10 == 0:
+    if i % 1 == 0:
         summary, loss_val, acc_val = unet.sess.run([unet.summary, unet.loss, unet.accuracy],
                 feed_dict={unet.x:image_x,
                            unet.t:image_t,
                            unet.keep_prob:1.0})
         print ('Step: %d, Loss: %f, Accuracy: %f'
                % (i, loss_val, acc_val))
+
+        if os.path.isdir('saver') == False:
+            os.mkdir('saver')
         unet.saver.save(unet.sess, os.path.join(os.getcwd(), 'saver/unet_session'), global_step=i)
-        unet.writer.add_summary(summary, i)
+        # unet.writer.add_summary(summary, i)
+
+
+output_sizex = unet.output_sizex
+output_sizey = unet.output_sizey
+num_class = unet.num_class
 
 timage = np.array(unet.sess.run([unet.tout], feed_dict = {unet.x:image_x, unet.t:image_t, unet.keep_prob:1.0}))
 timage = timage.reshape(-1, output_sizex, output_sizey, num_class)
---
+++ unet.py
@@ -0,0 +1,141 @@
+import tensorflow as tf
+import numpy as np
+import os
+import shutil
+import batch
+from collections import OrderedDict
+from libs import (get_variable, get_conv, get_bias, get_pool, get_crop, get_concat, get_deconv2)
+
+
+class UNET:
+    #テストデータ[-1, input_sizex, input_sizey]
+    #ラベル[-1, input_sizex, input_sizey, 2]
+    #初期化はinput_sizex, input_sizey, num_classは必ず指定、depthとかは"depth ="で行う 
+    #input_sizex, input_sizeyを変に設定するとpoolで死ぬ可能性あり。
+
+    def __init__(self, input_sizex , input_sizey, num_class, depth = 4, layers_default = 8):
+        with tf.Graph().as_default():
+
+            self.depth = depth
+            self.layers_default = layers_default
+            self.input_sizex = input_sizex
+            self.input_sizey = input_sizey
+            self.num_class = num_class
+
+            self.prepare_model()
+            self.prepare_session()
+
+    def prepare_model(self):
+        depth = self.depth
+        layers_default = self.layers_default
+        input_sizex = self.input_sizex
+        input_sizey = self.input_sizey
+        num_class = self.num_class
+
+        with tf.name_scope('input'):
+            x = tf.placeholder(tf.float32, [None, input_sizex, input_sizey])
+            h_pool = tf.reshape(x, [-1, input_sizex, input_sizey, 1])
+
+        with tf.name_scope('contracting'):
+            layers = layers_default
+            h_array = OrderedDict()
+
+            output_sizex = input_sizex
+            output_sizey = input_sizey
+
+            for i in range(depth):
+                if i == 0:
+                    filter1 = get_variable([3, 3, 1, layers])
+                else:
+                    filter1 = get_variable([3, 3, layers // 2, layers])
+                h1 = get_conv(h_pool, filter1, 1, 'VALID')
+
+                filter2 = get_variable([3, 3, layers, layers])
+                h2 = get_conv(h1, filter2, 1, 'VALID')
+
+                h_array[i] = h2
+                h_pool = get_pool(h2, 2)
+
+                layers = layers * 2
+                output_sizex = (output_sizex - 4) // 2
+                output_sizey = (output_sizey - 4) // 2
+
+
+        with tf.name_scope('floor'):
+            filter5_1 = get_variable([3, 3, layers // 2, layers])
+            h5_1 = get_conv(h_pool, filter5_1, 1, 'VALID')
+
+            filter5_2 = get_variable([3, 3, layers, layers])
+            h_pool = get_conv(h5_1, filter5_2, 1, 'VALID')
+
+            output_sizex = output_sizex - 4
+            output_sizey = output_sizey - 4
+
+        with tf.name_scope('expanding'):
+            for i in range(depth):
+                filter5 = get_variable([2, 2, layers // 2, layers])
+                h3 = get_deconv2(h_pool, filter5)
+
+                hcat = get_concat(h_array[depth - 1- i], h3)
+                filter1 = get_variable([3, 3, layers, layers // 2])
+                h1 = get_conv(hcat, filter1, 1, 'VALID')
+
+                filter2 = get_variable([3, 3, layers // 2, layers // 2])
+                h_pool = get_conv(h1, filter2, 1, 'VALID')
+
+                layers = layers // 2
+                output_sizex = output_sizex * 2 - 4
+                output_sizey = output_sizey * 2 - 4
+
+
+        with tf.name_scope('dropout'):
+            keep_prob = tf.placeholder(tf.float32)
+            h_pool = tf.nn.dropout(h_pool, keep_prob)
+
+        with tf.name_scope('softmax'):
+
+            filter1_3 = get_variable([1, 1, layers, num_class])
+            h_pool = get_conv(h_pool, filter1_3, 1, 'VALID') 
+
+            h_pool_flat = tf.reshape(h_pool, [-1, num_class])
+
+            result = tf.nn.softmax(h_pool_flat)
+
+        with tf.name_scope('optimizer'):
+            t = tf.placeholder(tf.float32, [None, input_sizex, input_sizey, num_class])
+            tcrop = get_crop(t, [output_sizex, output_sizey])
+            tout = tf.reshape(tcrop, [-1, num_class])
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tout,logits=result))
+            train_step = tf.train.MomentumOptimizer(learning_rate = 0.02, momentum = 0.02).minimize(loss)
+
+        with tf.name_scope('evaluator'):
+            correct_prediction = tf.equal(tf.argmax(result, 1), tf.argmax(tout, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        tf.summary.scalar("loss", loss)
+        tf.summary.scalar("accuracy", accuracy)
+        
+        self.x, self.t, self.result, self.keep_prob = x, t, result, keep_prob
+        self.train_step = train_step
+        self.loss = loss
+        self.tout = tout
+        self.result = result
+        self.accuracy = accuracy
+        self.output_sizex = output_sizex
+        self.output_sizey = output_sizey
+
+    def prepare_session(self):
+        sess = tf.InteractiveSession()
+        sess.run(tf.global_variables_initializer())
+        summary = tf.summary.merge_all()
+
+        saver = tf.train.Saver()
+        if os.path.isdir('/tmp/logs'):
+            shutil.rmtree('/tmp/logs')
+        writer = tf.summary.FileWriter("/tmp/logs", sess.graph)
+        
+        self.sess = sess
+        self.summary = summary
+        self.writer = writer
+        self.saver = saver
+
